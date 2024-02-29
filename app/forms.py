4b9624de47f257ea53f64c8c74ce2c93f6e8740c
Nicholas Joiles <kemanijoiles@gmail.com>
2024-02-29 01:02:51 -0500

    I have made some amount of progress

--- app/forms.py
+++ app/forms.py
@@ -3,9 +3,10 @@ from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import StringField, TextAreaField
 from wtforms.validators import InputRequired, Email
 
+#This is the name that will bbe displayed above your input fields
 
 class ContactForm(FlaskForm):
     name = StringField('Name', validators=[InputRequired()])
     email = StringField('Email', validators=[InputRequired(), Email()])
-    subject = StringField('Contact', validators=[InputRequired(), Email()])
+    subject = StringField('Subject ', validators=[InputRequired()])
     message = TextAreaField('Message', validators=[InputRequired()])
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from app import mail 
+from flask_mail import Message
+
 
 
 # When using Flask-WTF we need to import the Form Class that we created in forms.py
@@ -21,12 +24,59 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Nicholas Joiles")
 
+
+@app.route('/tester-form', methods = ['GET', 'POST'])
+def tester():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        message = request.form['message']
+
+        return render_template('results.html',
+                                firstname=firstname,
+                                lastname=lastname,
+                                email=email,
+                                message=message)
+
+    return render_template('tester.html')
+
 @app.route('/contact/', methods = ['GET', 'POST' ])
 def contact():
 
-    joiles_form = ContactForm() 
+    joiles_form = ContactForm()
+    # This shows the debug message in your development server
+    app.logger.debug(joiles_form)
+    if request.method == 'POST':
+        #This function validate_on_submit() checks if the generated string matches what is has in it's session 
+  
+        if joiles_form.validate_on_submit():
+            # Note the difference when retrieving form data using Flask-WTF
+            # Here we use joiles_form.name instead of request.form['firstname']
+            alias = joiles_form.name.data
+            elec_mail =  joiles_form.email.data
+            subs = joiles_form.subject.data
+            sms =  joiles_form.message.data
+
+            msg = Message(subs,  
+            sender=(alias, elec_mail), 
+            recipients=["to@example.com"]) 
+            msg.body = sms 
+            mail.send(msg)
+
+            flash('Completed', 'success')
+            return redirect(url_for('home'))
+            #This return is to improves user experience. gives the user feedback based on form completed.
+            # return render_template('results.html', name = alias,
+            #                                         email = elec_mail,
+            #                                         subj = subs,
+            #                                         mess = sms)
+        flash_errors(joiles_form)
+    
     """Render the website's contact page"""
-    return render_template('contact.html')
+    #This return Displays the form if the method is a GET request 
+    return render_template('contact.html', temp = joiles_form )
+    
 
 
 
@@ -36,6 +86,7 @@ def contact():
 
 
 # Flash errors from the form if validation fails
+# This loops over all form errors in the form and flash the error when the page is reloaded 
 def flash_errors(form):
     for field, errors in form.errors.items():
         for error in errors:
